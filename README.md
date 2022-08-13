2f8d31f92091e1b941288685b27b11d651c8be9d
GenesisHacks <109468312+GenesisHacks@users.noreply.github.com>
2022-08-13 09:27:20 +0100

    Initial upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# Simple Autofish
+
+## Description
+
+Very simple autofish mod.
+
+This mod currently has no options or settings.
+
+## How to use
+
+Just put the jar file in your mods folder and start fishing :)
---
+++ src/main/java/net/genesis/autofish/mixin/AutofishMixin.java
@@ -0,0 +1,32 @@
+package net.genesis.autofish.mixin;
+
+
+import net.minecraft.util.Hand;
+import net.minecraft.entity.projectile.FishingBobberEntity;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(FishingBobberEntity.class)
+public abstract class AutofishMixin {
+	@Shadow
+	private boolean caughtFish;
+
+	@Inject(at = {@At("TAIL")}, method = {"onTrackedDataSet"})
+	public void onTrackedDataSet(TrackedData<?> data, CallbackInfo ci) throws InterruptedException {
+
+
+		MinecraftClient client = MinecraftClient.getInstance();
+		if (caughtFish) {
+
+
+			client.interactionManager.interactItem(client.player, Hand.MAIN_HAND);
+			Thread.sleep(5L);
+			client.interactionManager.interactItem(client.player, Hand.MAIN_HAND);
+		}
+	}
+}
